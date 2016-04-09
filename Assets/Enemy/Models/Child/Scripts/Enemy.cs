8426bb3bf02088c123633390d400e97c09d6edf3
Victor Nicolas <vk.007.nk@gmail.com>
2016-04-09 13:25:20 -0500

    Update Adding Enemies

---
+++ Assets/Enemy/Models/Child/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    public int hp;
+    public int damege;
+    public GameObject cont;
+    TotalEnemies te;
+
+	// Use this for initialization
+	void Start () {
+        cont = GameObject.FindGameObjectWithTag("CameraController");
+        te = cont.GetComponent<TotalEnemies>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(hp <= 0)
+        {
+            te.OnDestrction();
+            Destroy(gameObject);
+        }
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bullet")
+        {
+            OnDamage(1);
+        }
+    }
+    public void OnDamage(int value)
+    {
+        hp -= value;
+    }
+}
---
+++ Assets/Enemy/Models/Child/Scripts/PlayerDetection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDetection : MonoBehaviour {
+    public float maxDifference;
+    public GameObject playerTarget;
+    float difference;
+    public bool isDetected = false;
+
+    // Use this for initialization
+    void Start () {
+        playerTarget = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        difference = Vector3.Distance(transform.position, playerTarget.transform.position);
+        if(difference <= maxDifference)
+        {
+            isDetected = true;
+        }
+    }
+    public void ChangeDetection()
+    {
+        isDetected = false;
+    }
+}

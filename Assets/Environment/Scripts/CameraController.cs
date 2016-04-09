65c27cd96ee0b535ddb3c26f5dd61536308b6520
Victor Nicolas <vk.007.nk@gmail.com>
2016-04-09 14:03:52 -0500

    Update environment

---
+++ Assets/Environment/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject maincamera;
+    public GameObject firstcamera;
+    public GameObject playertarget;
+    Vector3 offset1;
+    Vector3 offset2;
+    public bool activecam = false;
+    // Use this for initialization
+    void Start () {
+        firstcamera = GameObject.Find("FirstPersonCamera");
+        firstcamera.SetActive(false);
+        maincamera = GameObject.FindGameObjectWithTag("MainCamera");
+        playertarget = GameObject.FindGameObjectWithTag("Player");
+        offset1 = maincamera.transform.position - playertarget.transform.position;
+        offset2 = firstcamera.transform.position - playertarget.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            activecam = !activecam;
+
+        }
+        if (activecam)
+        {
+            firstcamera.SetActive(true);
+            maincamera.SetActive(false);
+            
+        }
+        else
+        {
+            firstcamera.SetActive(false);
+            maincamera.SetActive(true);
+            
+        }
+        if (playertarget == null)
+        {
+            playertarget = GameObject.FindGameObjectWithTag("Player");
+        }
+        Vector3 desiredPosition1 = playertarget.transform.position + offset1;
+        maincamera.transform.position = desiredPosition1;
+
+        Vector3 desiredPosition2 = playertarget.transform.position + offset2;
+        firstcamera.transform.position = desiredPosition2;
+
+    }
+}
---
+++ Assets/Environment/Scripts/HpController.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HpController : MonoBehaviour {
+    public GameObject playerTarget;
+    PlayerController pc;
+
+    public RawImage resist1;
+    public RawImage resist2;
+    public RawImage resist3;
+    public RawImage resist4;
+    public RawImage resist5;
+    public RawImage resist6;
+    public RawImage resist7;
+    public RawImage resist8;
+    public RawImage resist9;
+    public RawImage resist10;
+    // Use this for initialization
+    void Start () {
+        playerTarget = GameObject.FindGameObjectWithTag("Player");
+        pc = playerTarget.GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	if(pc.hp == 10)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = true;
+            resist5.GetComponent<RawImage>().enabled = true;
+            resist6.GetComponent<RawImage>().enabled = true;
+            resist7.GetComponent<RawImage>().enabled = true;
+            resist8.GetComponent<RawImage>().enabled = true;
+            resist9.GetComponent<RawImage>().enabled = true;
+            resist10.GetComponent<RawImage>().enabled = true;
+        }
+        if (pc.hp == 9)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = true;
+            resist5.GetComponent<RawImage>().enabled = true;
+            resist6.GetComponent<RawImage>().enabled = true;
+            resist7.GetComponent<RawImage>().enabled = true;
+            resist8.GetComponent<RawImage>().enabled = true;
+            resist9.GetComponent<RawImage>().enabled = true;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 8)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = true;
+            resist5.GetComponent<RawImage>().enabled = true;
+            resist6.GetComponent<RawImage>().enabled = true;
+            resist7.GetComponent<RawImage>().enabled = true;
+            resist8.GetComponent<RawImage>().enabled = true;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 7)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = true;
+            resist5.GetComponent<RawImage>().enabled = true;
+            resist6.GetComponent<RawImage>().enabled = true;
+            resist7.GetComponent<RawImage>().enabled = true;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 6)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = true;
+            resist5.GetComponent<RawImage>().enabled = true;
+            resist6.GetComponent<RawImage>().enabled = true;
+            resist7.GetComponent<RawImage>().enabled = false;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 5)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = true;
+            resist5.GetComponent<RawImage>().enabled = true;
+            resist6.GetComponent<RawImage>().enabled = false;
+            resist7.GetComponent<RawImage>().enabled = false;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 4)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = true;
+            resist5.GetComponent<RawImage>().enabled = false;
+            resist6.GetComponent<RawImage>().enabled = false;
+            resist7.GetComponent<RawImage>().enabled = false;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 3)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = true;
+            resist4.GetComponent<RawImage>().enabled = false;
+            resist5.GetComponent<RawImage>().enabled = false;
+            resist6.GetComponent<RawImage>().enabled = false;
+            resist7.GetComponent<RawImage>().enabled = false;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 2)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = true;
+            resist3.GetComponent<RawImage>().enabled = false;
+            resist4.GetComponent<RawImage>().enabled = false;
+            resist5.GetComponent<RawImage>().enabled = false;
+            resist6.GetComponent<RawImage>().enabled = false;
+            resist7.GetComponent<RawImage>().enabled = false;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp == 1)
+        {
+            resist1.GetComponent<RawImage>().enabled = true;
+            resist2.GetComponent<RawImage>().enabled = false;
+            resist3.GetComponent<RawImage>().enabled = false;
+            resist4.GetComponent<RawImage>().enabled = false;
+            resist5.GetComponent<RawImage>().enabled = false;
+            resist6.GetComponent<RawImage>().enabled = false;
+            resist7.GetComponent<RawImage>().enabled = false;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+        if (pc.hp <= 0)
+        {
+            resist1.GetComponent<RawImage>().enabled = false;
+            resist2.GetComponent<RawImage>().enabled = false;
+            resist3.GetComponent<RawImage>().enabled = false;
+            resist4.GetComponent<RawImage>().enabled = false;
+            resist5.GetComponent<RawImage>().enabled = false;
+            resist6.GetComponent<RawImage>().enabled = false;
+            resist7.GetComponent<RawImage>().enabled = false;
+            resist8.GetComponent<RawImage>().enabled = false;
+            resist9.GetComponent<RawImage>().enabled = false;
+            resist10.GetComponent<RawImage>().enabled = false;
+        }
+    }
+}

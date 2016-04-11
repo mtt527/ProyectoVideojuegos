c5819770f8ad1a88f9bf5e29468bd0592384d3a5
Raul Tc <A01260402@itesm.mx>
2016-04-10 21:19:40 -0500

    start menu added

---
+++ Assets/UI/StartMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public GameObject StartButton;
+	public GameObject InfoButton;
+
+	public GameObject About;
+	public GameObject goBack;
+	// Use this for initialization
+	void Start () {
+	
+		About.SetActive (false);
+		goBack.SetActive (false);
+	}
+
+	public void LoadLevel (string levelname){
+		
+		Application.LoadLevel (levelname);
+	}
+
+	public void ShowInfo () {
+
+		StartButton.SetActive (false);
+		InfoButton.SetActive (false);
+
+		About.SetActive (true);
+		goBack.SetActive (true);
+	}
+
+	public void Back () {
+
+		StartButton.SetActive (true);
+		InfoButton.SetActive (true);
+
+		About.SetActive (false);
+		goBack.SetActive (false);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
